7bcda38897b76009387cf30e596fcbdfa99c545c
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2021-12-24 16:00:37 +0300

    Menu demo update

--- src/app/demo/view/menu/paymentdemo.component.ts
+++ src/app/demo/view/menu/paymentdemo.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-paymentdemo',
   template: `
-    <div class="flex align-items-center py-5 px-3">
+    <div class="flex flex-column align-items-center py-5 px-3">
       <i class="pi pi-fw pi-money-bill mr-2 text-2xl"></i>
-      <p class="m-0 text-lg">Payment Component Content via Child Route</p>
+      <p class="m-0 mt-5 text-center text-lg">Payment Component Content via Child Route</p>
     </div>
   `
 })
--- src/app/demo/view/menu/personaldemo.component.ts
+++ src/app/demo/view/menu/personaldemo.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-personaldemo',
   template: `
-  <div class="flex align-items-center py-5 px-3">
+  <div class="flex flex-column align-items-center py-5 px-3">
     <i class="pi pi-fw pi-user mr-2 text-2xl"></i>
-    <p class="m-0 text-lg">Personal Component Content via Child Route</p>
+    <p class="mt-5 text-center text-lg">Personal Component Content via Child Route</p>
   </div>
   `
 })
