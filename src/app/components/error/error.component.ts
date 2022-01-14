ca391ef1d660b0c7beea966891d521b007ae6fe7
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2022-01-14 19:22:19 +0300

    remove css and unused import

--- src/app/components/error/error.component.ts
+++ src/app/components/error/error.component.ts
@@ -1,15 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
-  styleUrls: ['./error.component.scss']
 })
-export class ErrorComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class ErrorComponent { }
