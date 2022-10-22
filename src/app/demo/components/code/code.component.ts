6d7da9c04c1e04549811b420f346faad8965a378
Cagatay Civici <cagatay.civici@gmail.com>
2022-10-22 15:38:02 +0300

    Removed prism and added cosmetics

--- src/app/demo/components/code/code.component.ts
+++ src/app/demo/components/code/code.component.ts
@@ -1,30 +1,14 @@
-import { Component, ElementRef, AfterViewInit, Input, NgModule, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-code',
     template: `
-        <pre [ngClass]="'language-' + lang"><code #code><ng-content></ng-content>
-</code></pre>
-    `,
-    styleUrls: ['./code.component.scss'],
-    encapsulation: ViewEncapsulation.None
+        <pre class="surface-ground p-5 border-round mb-3 overflow-auto"><code class="-mt-4 p-0 line-height-3 block" [ngStyle]="{'font-family': 'monaco, Consolas, monospace'}"><ng-content></ng-content></code></pre>
+    `
 })
-export class AppCodeComponent implements AfterViewInit {
+export class AppCodeComponent {
 
-    @Input() lang = 'markup';
-
-    @ViewChild('code') codeViewChild!: ElementRef;
-
-    constructor(public el: ElementRef) { }
-
-    ngAfterViewInit() {
-        // @ts-ignore
-        if (window['Prism']) {
-            // @ts-ignore
-            window['Prism'].highlightElement(this.codeViewChild.nativeElement);
-        }
-    }
 }
 
 @NgModule({
