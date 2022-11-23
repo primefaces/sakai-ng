3f82d2c39a37293b0f1767fc9cfb25a679460736
Cagatay Civici <cagatay.civici@gmail.com>
2022-11-23 10:00:51 +0300

    Fixed code display overflow

--- src/app/demo/components/code/code.component.ts
+++ src/app/demo/components/code/code.component.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 @Component({
     selector: 'app-code',
     template: `
-        <pre class="surface-ground p-5 border-round mb-3 overflow-auto"><code class="-mt-4 p-0 line-height-3 block" [ngStyle]="{'font-family': 'monaco, Consolas, monospace'}"><ng-content></ng-content></code></pre>
+        <pre class="surface-ground p-5 border-round mb-3"><code class="-mt-4 p-0 line-height-3 block overflow-auto" [ngStyle]="{'font-family': 'monaco, Consolas, monospace'}"><ng-content></ng-content></code></pre>
     `
 })
 export class AppCodeComponent {
