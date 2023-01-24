d189771df47e8361fa52dfe04ff36f8dfbc47fec
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2023-01-24 16:00:19 +0300

    Fix typo

--- src/app/demo/components/primeblocks/blocks/blocks.component.ts
+++ src/app/demo/components/primeblocks/blocks/blocks.component.ts
@@ -10,7 +10,7 @@ export class BlocksComponent{
     <div class="col-12 md:col-6 p-6 text-center md:text-left flex align-items-center ">
         <section>
             <span class="block text-6xl font-bold mb-1">Create the screens your</span>
-            <div class="text-6xl text-primary font-bold mb-3">your visitors deserve to see</div>
+            <div class="text-6xl text-primary font-bold mb-3">visitors deserve to see</div>
             <p class="mt-0 mb-4 text-700 line-height-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 
             <button pButton pRipple label="Learn More" type="button" class="mr-3 p-button-raised"></button>
