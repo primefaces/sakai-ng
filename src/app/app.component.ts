b79258ea6d72c6c1cb39afb3b3044c220ec5dad3
Justin McLellan <justin.mclellan99@gmail.com>
2023-06-11 07:45:55 -0500

    step 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    standalone: true,
+    imports: [RouterOutlet]
 })
 export class AppComponent implements OnInit {
 
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,30 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
 import { environment } from './environments/environment';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+import { ProductService } from './app/demo/service/product.service';
+import { PhotoService } from './app/demo/service/photo.service';
+import { NodeService } from './app/demo/service/node.service';
+import { IconService } from './app/demo/service/icon.service';
+import { EventService } from './app/demo/service/event.service';
+import { CustomerService } from './app/demo/service/customer.service';
+import { CountryService } from './app/demo/service/country.service';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(AppRoutingModule),
+        { provide: LocationStrategy, useClass: HashLocationStrategy },
+        CountryService, CustomerService, EventService, IconService, NodeService,
+        PhotoService, ProductService
+    ]
+})
   .catch(err => console.error(err));
