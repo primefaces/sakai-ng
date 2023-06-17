d31d69989a22b7a3398e649a9db9586d909ecd4a
Justin McLellan <justin.mclellan99@gmail.com>
2023-06-17 08:33:36 -0500

    remove hash routing and enable anchor scrolling

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { environment } from './environments/environment';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { APP_ROUTES } from './app/app.routes';
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations'
 import { HttpClientModule } from '@angular/common/http';
@@ -14,8 +13,10 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(RouterModule.forRoot(APP_ROUTES), HttpClientModule),
-        provideAnimations(),
-        { provide: LocationStrategy, useClass: HashLocationStrategy },
+        importProvidersFrom(RouterModule.forRoot(APP_ROUTES, {
+            scrollPositionRestoration: 'enabled',
+            anchorScrolling: 'enabled'
+          }), HttpClientModule),
+        provideAnimations()
     ],
 }).catch((err) => console.error(err));
