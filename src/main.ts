34fa6d2974d3561eac87975e4dedf9ca1eea2121
Justin McLellan <justin.mclellan99@gmail.com>
2023-06-17 07:59:03 -0500

    rename app-routes file

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { environment } from './environments/environment';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { APP_ROUTES } from './app/app-routing.module';
+import { APP_ROUTES } from './app/app.routes';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations'
