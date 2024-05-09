93aca02eb0d7810156a1ee8e8c6af1e1f94f297d
qwa-nicoleciccolani <106842776+NickHirata@users.noreply.github.com>
2024-05-09 15:17:41 -0300

    Ajuste layout tela inicial e login

--- src/app/demo/components/dashboard/dashboard.module.ts
+++ src/app/demo/components/dashboard/dashboard.module.ts
@@ -3,18 +3,23 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { DashboardComponent } from './dashboard.component';
 import { ChartModule } from 'primeng/chart';
+import { DialogModule } from 'primeng/dialog';
 import { MenuModule } from 'primeng/menu';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { StyleClassModule } from 'primeng/styleclass';
 import { PanelMenuModule } from 'primeng/panelmenu';
 import { DashboardsRoutingModule } from './dashboard-routing.module';
+import { Component } from '@angular/core';
+
+
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
         ChartModule,
+        DialogModule,
         MenuModule,
         TableModule,
         StyleClassModule,
@@ -22,6 +27,14 @@ import { DashboardsRoutingModule } from './dashboard-routing.module';
         ButtonModule,
         DashboardsRoutingModule
     ],
+
     declarations: [DashboardComponent]
 })
-export class DashboardModule { }
+@Component({
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.css']
+  })
+export class DashboardModule {
+    display: boolean = false;
+}
