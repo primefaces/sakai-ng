bb0d05e7e15f8a62a83311a4c2077ad0aa7ece50
Darkuna <elias.j.walder@student.uibk.ac.at>
2024-12-16 14:12:51 +0100

    changed info boxes

--- src/app/demo/components/dashboard/dashboard.component.ts
+++ src/app/demo/components/dashboard/dashboard.component.ts
@@ -1,111 +1,82 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { MenuItem } from 'primeng/api';
-import { Product } from '../../api/product';
-import { ProductService } from '../../service/product.service';
-import { Subscription, debounceTime } from 'rxjs';
-import { LayoutService } from 'src/app/layout/service/app.layout.service';
+import {Component, OnInit,} from '@angular/core';
 import {CalendarOptions} from "@fullcalendar/core";
-import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from "@fullcalendar/interaction";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+
+class InfoBox {
+    icon: string
+    header: string
+    value: any
+    highlight: string
+    fin: string
+}
 
 @Component({
     templateUrl: './dashboard.component.html',
 })
-export class DashboardComponent implements OnInit, OnDestroy {
-    calendarOptions: CalendarOptions = {
-        initialView: 'dayGridMonth',
-        plugins: [dayGridPlugin]
+export class DashboardComponent implements OnInit{
+    calendarOptions :CalendarOptions = {
+        plugins: [
+            interactionPlugin,
+            dayGridPlugin,
+            timeGridPlugin,
+        ],
+        headerToolbar: {
+            left: '',
+            center: '',
+            right: ''
+        },
+        initialView: 'timeGridWeek',
+        weekends: false,
+        editable: false,
+        selectable: false,
+        selectMirror: true,
+        dayMaxEvents: true,
+        allDaySlot: false,
+        height: "85vh",
+        eventBackgroundColor: "#666666",
+        eventBorderColor: "#050505",
+        eventTextColor: "var(--system-color-primary-white)",
+        slotMinTime: '08:00',
+        slotMaxTime: '22:00',
+        slotDuration: '00:15',
+        slotLabelInterval: '00:30',
+        dayHeaderFormat: {weekday: 'long'},
+        eventOverlap: true,
+        slotEventOverlap: true,
+        nowIndicator: false,
+        //eventClick: this.showHoverDialog.bind(this),
     };
 
-    items!: MenuItem[];
+    infos!: InfoBox[];
 
-    products!: Product[];
-
-    chartData: any;
-
-    chartOptions: any;
-
-    subscription!: Subscription;
-
-    constructor(private productService: ProductService, public layoutService: LayoutService) {
-        this.subscription = this.layoutService.configUpdate$
-        .pipe(debounceTime(25))
-        .subscribe((config) => {
-            this.initChart();
-        });
+    constructor(
+    ) {
     }
 
-    ngOnInit() {
-        this.initChart();
-        this.productService.getProductsSmall().then(data => this.products = data);
-
-        this.items = [
-            { label: 'Add New', icon: 'pi pi-fw pi-plus' },
-            { label: 'Remove', icon: 'pi pi-fw pi-minus' }
-        ];
-    }
-
-    initChart() {
-        const documentStyle = getComputedStyle(document.documentElement);
-        const textColor = documentStyle.getPropertyValue('--text-color');
-        const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
-        const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
-
-        this.chartData = {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            datasets: [
-                {
-                    label: 'First Dataset',
-                    data: [65, 59, 80, 81, 56, 55, 40],
-                    fill: false,
-                    backgroundColor: documentStyle.getPropertyValue('--bluegray-700'),
-                    borderColor: documentStyle.getPropertyValue('--bluegray-700'),
-                    tension: .4
-                },
-                {
-                    label: 'Second Dataset',
-                    data: [28, 48, 40, 19, 86, 27, 90],
-                    fill: false,
-                    backgroundColor: documentStyle.getPropertyValue('--green-600'),
-                    borderColor: documentStyle.getPropertyValue('--green-600'),
-                    tension: .4
-                }
-            ]
-        };
-
-        this.chartOptions = {
-            plugins: {
-                legend: {
-                    labels: {
-                        color: textColor
-                    }
-                }
+    ngOnInit(): void {
+        this.infos = [
+            {
+                icon: 'pi pi-shopping-cart text-blue-500 text-xl',
+                header: 'Orders', value : 152,
+                highlight : '24 new ', fin: 'since last visit'
             },
-            scales: {
-                x: {
-                    ticks: {
-                        color: textColorSecondary
-                    },
-                    grid: {
-                        color: surfaceBorder,
-                        drawBorder: false
-                    }
-                },
-                y: {
-                    ticks: {
-                        color: textColorSecondary
-                    },
-                    grid: {
-                        color: surfaceBorder,
-                        drawBorder: false
-                    }
-                }
+            {
+                icon: 'pi pi-map-marker text-orange-500 text-xl',
+                header: 'Revenue', value : '$2.100',
+                highlight : '%52+ ', fin: 'since last week'
+            },
+            {
+                icon: 'pi pi-inbox text-cyan-500 text-xl',
+                header: 'Customers', value : 28441,
+                highlight : '520 ', fin: 'newly registered'
+            },
+            {
+                icon: '', header: 'Comments', value : '152 Unread',
+                highlight : '85 ', fin: 'responded'
             }
-        };
+        ]
     }
 
-    ngOnDestroy() {
-        if (this.subscription) {
-            this.subscription.unsubscribe();
-        }
-    }
 }
