5678bbd553fb44159d31b7335e36e2884d08dffb
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2025-01-03 12:52:44 +0300

    Add app.component & app.config.ts

---
+++ src/app.config.ts
@@ -0,0 +1,16 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { ApplicationConfig } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { routes } from '@/src/routes';
+
+export const appConfig: ApplicationConfig = {
+    providers: [
+        provideRouter(routes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), withEnabledBlockingInitialNavigation()),
+        provideHttpClient(withFetch()),
+        provideAnimationsAsync(),
+        providePrimeNG({ theme: {preset: Aura, options: { darkModeSelector: '.app-dark'}}, ripple: false, inputStyle: 'outlined' })
+    ]
+};
