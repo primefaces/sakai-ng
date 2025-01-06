cedbba30ae9a2b839f1369f4756085d7062c4793
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-01-06 15:38:18 +0300

    update routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,22 @@
 import { Routes } from '@angular/router';
-import { AppLayout} from '@/src/layout/applayout';
+import { AppLayout } from '@/src/layout/applayout';
 import { Documentation } from '@/src/views/pages/documentation';
 import { Dashboard } from '@/src/views/dashboard';
+import { Landing } from './views/pages/landing';
+import { Notfound } from './views/pages/notfound';
 
 export const routes: Routes = [
-    { path: '', component: AppLayout,
+    {
+        path: '',
+        component: AppLayout,
         children: [
             { path: '', component: Dashboard },
             { path: 'uikit', loadChildren: () => import('./views/uikit/uikit.routes') },
             { path: 'documentation', component: Documentation },
-            { path: 'pages', loadChildren: () => import('./views/pages/pages.routes') },
+            { path: 'pages', loadChildren: () => import('./views/pages/pages.routes') }
         ]
     },
-    {path: 'auth', loadChildren: () => import('./views/pages/auth/auth.routes') },
-
+    { path: 'landing', component: Landing },
+    { path: 'notfound', component: Notfound },
+    { path: 'auth', loadChildren: () => import('./views/pages/auth/auth.routes') }
 ];
--- src/views/pages/pages.routes.ts
+++ src/views/pages/pages.routes.ts
@@ -1,15 +1,11 @@
 import { Routes } from '@angular/router';
 import { Documentation } from '@/src/views/pages/documentation';
 import { Crud } from '@/src/views/pages/crud';
-import { Landing } from '@/src/views/pages/landing';
 import { Empty } from '@/src/views/pages/empty';
-import { Notfound } from '@/src/views/pages/notfound';
 
 export default [
-  { path: 'documentation', component: Documentation},
-  { path: 'crud', component: Crud},
-  { path: 'landing', component: Landing},
-  { path: 'empty', component: Empty},
-  { path: 'notfound', component: Notfound},
-  { path: '**', redirectTo: '/notfound' }
+    { path: 'documentation', component: Documentation },
+    { path: 'crud', component: Crud },
+    { path: 'empty', component: Empty },
+    { path: '**', redirectTo: '/notfound' }
 ] as Routes;
