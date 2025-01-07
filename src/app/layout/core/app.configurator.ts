9049221aada233c7507c8dbfaaa8d831a18eeaba
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-01-07 16:39:58 +0300

    appconfigurator | update host class

--- src/app/layout/core/app.configurator.ts
+++ src/app/layout/core/app.configurator.ts
@@ -88,7 +88,7 @@ declare type SurfacesType = {
         </div>
     `,
     host: {
-        class: 'hidden absolute top-[3.25rem] right-0 w-64 p-4 bg-surface-0 dark:bg-surface-900 border border-surface rounded-border origin-top shadow-[0px_3px_5px_rgba(0,0,0,0.02),0px_0px_2px_rgba(0,0,0,0.05),0px_1px_4px_rgba(0,0,0,0.08)]'
+        class: 'hidden absolute top-[3.25rem] right-0 w-72 p-4 bg-surface-0 dark:bg-surface-900 border border-surface rounded-border origin-top shadow-[0px_3px_5px_rgba(0,0,0,0.02),0px_0px_2px_rgba(0,0,0,0.05),0px_1px_4px_rgba(0,0,0,0.08)]'
     }
 })
 export class AppConfigurator {
