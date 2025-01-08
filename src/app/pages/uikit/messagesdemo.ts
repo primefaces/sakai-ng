fa2e167e3fd3e2fe563ed04cdc64c669f56c6162
Mehmet Çetin <92744169+mehmetcetin01140@users.noreply.github.com>
2025-01-08 12:02:11 +0300

    update messagesdemo

--- src/app/pages/uikit/messagesdemo.ts
+++ src/app/pages/uikit/messagesdemo.ts
@@ -21,6 +21,7 @@ import { FormsModule } from '@angular/forms';
                         <p-button (click)="showInfoViaToast()" label="Info" severity="info" />
                         <p-button (click)="showWarnViaToast()" label="Warn" severity="warn" />
                         <p-button (click)="showErrorViaToast()" label="Error" severity="danger" />
+                        <p-toast />
                     </div>
 
                     <div class="font-semibold text-xl mt-4 mb-4">Inline</div>
@@ -61,38 +62,18 @@ export class MessagesDemo {
     constructor(private service: MessageService) {}
 
     showInfoViaToast() {
-        this.service.add({ key: 'tst', severity: 'info', summary: 'Info Message', detail: 'PrimeNG rocks' });
+        this.service.add({ severity: 'info', summary: 'Info Message', detail: 'PrimeNG rocks' });
     }
 
     showWarnViaToast() {
-        this.service.add({ key: 'tst', severity: 'warn', summary: 'Warn Message', detail: 'There are unsaved changes' });
+        this.service.add({ severity: 'warn', summary: 'Warn Message', detail: 'There are unsaved changes' });
     }
 
     showErrorViaToast() {
-        this.service.add({ key: 'tst', severity: 'error', summary: 'Error Message', detail: 'Validation failed' });
+        this.service.add({ severity: 'error', summary: 'Error Message', detail: 'Validation failed' });
     }
 
     showSuccessViaToast() {
-        this.service.add({ key: 'tst', severity: 'success', summary: 'Success Message', detail: 'Message sent' });
-    }
-
-    showInfoViaMessages() {
-        this.msgs = [];
-        this.msgs.push({ severity: 'info', summary: 'Info Message', detail: 'PrimeNG rocks' });
-    }
-
-    showWarnViaMessages() {
-        this.msgs = [];
-        this.msgs.push({ severity: 'warn', summary: 'Warn Message', detail: 'There are unsaved changes' });
-    }
-
-    showErrorViaMessages() {
-        this.msgs = [];
-        this.msgs.push({ severity: 'error', summary: 'Error Message', detail: 'Validation failed' });
-    }
-
-    showSuccessViaMessages() {
-        this.msgs = [];
-        this.msgs.push({ severity: 'success', summary: 'Success Message', detail: 'Message sent' });
+        this.service.add({ severity: 'success', summary: 'Success Message', detail: 'Message sent' });
     }
 }
