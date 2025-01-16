029bd85db7773d5dd6721d52bf035c0a79ae36fa
Chris Blume <christopher.blume@gmx.de>
2025-01-16 15:16:29 +0100

    re-structure of login component and login logic

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,7 +1,7 @@
-import { effect, Injectable, signal } from '@angular/core';
+import { effect, Injectable, Signal, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, of, switchMap, tap } from 'rxjs';
-import { environment } from '../../../../environment/environment';
+import { catchError, Observable, of, switchMap, tap } from 'rxjs';
+import { environment } from '../../../environment/environment';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -11,6 +11,7 @@ export class AuthService {
     private apiURL: string = `${environment.apiUrl}/auth`;
     private tokenSignal = signal<string | null>(localStorage.getItem('token'));
     private isAuthenticatedSignal = signal<boolean>(!!this.tokenSignal());
+    private loginSuccess = signal<boolean>(false);
 
     constructor(private http: HttpClient) {
         effect(() => {
@@ -23,7 +24,7 @@ export class AuthService {
         });
     }
 
-    get isAuthenticated() {
+    get isAuthenticated(): Signal<boolean> {
         return this.isAuthenticatedSignal.asReadonly();
     }
 
@@ -31,37 +32,22 @@ export class AuthService {
         return this.tokenSignal();
     }
 
-    login(email: string, password: string): void {
-        console.log('email: ', email);
-        console.log('password: ', password);
-        this.http
-            .post<{ token: string }>(`${this.apiURL}/login`, { email, password })
+    login(username: string, password: string): Observable<boolean> {
+        return this.http
+            .post<{ token: string }>(`${this.apiURL}/login`, { username, password })
             .pipe(
                 tap((response) => {
                     if (response?.token) {
                         this.setToken(response.token); // Store the token
-                        console.log('token: ', this.getToken())
+                        console.log('token: ', this.getToken());
                     }
                 }),
                 switchMap(() => this.validateToken()) // Validate the token after storing it
-            )
-            .subscribe({
-                next: (isValid) => {
-                    if (isValid) {
-                        console.log('Navigating to /workouts');
-                        // this.router.navigate(['/workouts']);
-                    }
-                },
-                error: (err) => {
-                    console.error('Login failed:', err);
-                    this.logout();
-                },
-            });
+            );
     }
 
     logout(): void {
         this.setToken(null);
-        // this.router.navigate(['/login']);
     }
 
     private setToken(token: string | null): void {
@@ -81,9 +67,10 @@ export class AuthService {
         this.isAuthenticatedSignal.set(false);
     }
 
-    private validateToken() {
+    validateToken(): Observable<boolean> {
         const token = this.getToken();
         if (!token) {
+            this.loginSuccess.set(false);
             this.logout();
             return of(false);
         }
@@ -95,11 +82,13 @@ export class AuthService {
             .pipe(
                 tap(() => {
                     console.log('Token is valid');
+                    this.loginSuccess.set(true);
                     this.isAuthenticatedSignal.set(true); // Update the signal on success
                 }),
                 map(() => true), // Return `true` when the validation is successful
                 catchError(() => {
                     this.logout();
+                    this.loginSuccess.set(false);
                     return of(false); // Return `false` on error
                 })
             );
