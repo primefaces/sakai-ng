9a43e22de529a4d5e5631180beef59ec08767030
Hayyan <hayyan@chb-technologies.ch>
2025-01-29 18:30:54 +0400

    Add TopBarComponent with dark mode toggle and layout integration

--- src/app/layout/component/app.layout.ts
+++ src/app/layout/component/app.layout.ts
@@ -2,15 +2,16 @@ import { Component, Renderer2, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import { filter, Subscription } from 'rxjs';
-import { AppTopbar } from './app.topbar';
+
 import { AppSidebar } from './app.sidebar';
 import { AppFooter } from './app.footer';
 import { LayoutService } from '../service/layout.service';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @Component({
     selector: 'app-layout',
     standalone: true,
-    imports: [CommonModule, AppTopbar, AppSidebar, RouterModule, AppFooter],
+    imports: [CommonModule, TopBarComponent, AppSidebar, RouterModule, AppFooter],
     template: `<div class="layout-wrapper" [ngClass]="containerClass">
         <app-topbar></app-topbar>
         <app-sidebar></app-sidebar>
@@ -30,7 +31,7 @@ export class AppLayout {
 
     @ViewChild(AppSidebar) appSidebar!: AppSidebar;
 
-    @ViewChild(AppTopbar) appTopBar!: AppTopbar;
+    @ViewChild(TopBarComponent) appTopBar!: TopBarComponent;
 
     constructor(
         public layoutService: LayoutService,
