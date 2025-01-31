670ff9b93f023b6eab45cc1315b32ae1560a1c08
Chris Blume <christopher.blume@gmx.de>
2025-01-31 15:34:00 +0100

    clean up

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://crossfit-tracker-952680578606.us-central1.run.app/api',
     supabaseUrl: 'https://eavczlkvgdabkwxxulbo.supabase.co',
     supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhdmN6bGt2Z2RhYmt3eHh1bGJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc2MTg3NzcsImV4cCI6MjA1MzE5NDc3N30.g7BYJYx8_aw5H2rM_WOMca7LV_fF2M8Ogm42ZsC9EBY',
 };
