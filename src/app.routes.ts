8998d801e3948da5ceb042d8974ce58e4c5f7810
caoquyenis <58635793+caoquyenis@users.noreply.github.com>
2026-02-10 14:17:47 +0700

    feat: add layout showcase component and update routing and menu

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -3,6 +3,7 @@ import { AppLayout } from './app/layout/component/app.layout';
 import { Dashboard } from './app/pages/dashboard/dashboard';
 import { Documentation } from './app/pages/documentation/documentation';
 import { Landing } from './app/pages/landing/landing';
+import { Layout } from './app/pages/layout/layout';
 import { Notfound } from './app/pages/notfound/notfound';
 
 export const appRoutes: Routes = [
@@ -17,6 +18,7 @@ export const appRoutes: Routes = [
         ]
     },
     { path: 'landing', component: Landing },
+    { path: 'layout', component: Layout },
     { path: 'notfound', component: Notfound },
     { path: 'auth', loadChildren: () => import('./app/pages/auth/auth.routes') },
     { path: '**', redirectTo: '/notfound' }
--- src/app/layout/component/app.menu.ts
+++ src/app/layout/component/app.menu.ts
@@ -93,6 +93,11 @@ export class AppMenu {
                         label: 'Empty',
                         icon: 'pi pi-fw pi-circle-off',
                         routerLink: ['/pages/empty']
+                    },
+                    {
+                        label: 'Layout',
+                        icon: 'pi pi-fw pi-objects-column',
+                        routerLink: ['/layout']
                     }
                 ]
             },
